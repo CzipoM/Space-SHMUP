26b73b0ea629ac55d6ca02f555170a5a2e222f75
CzipoM <Mikecz613@aol.com>
2017-10-20 12:15:24 -0400

    Shield displays properly, ship should take damage, main ship z coordinates offset by 4.3 for unknown reason

--- Assets/__Scripts/Hero.cs
+++ Assets/__Scripts/Hero.cs
@@ -8,8 +8,8 @@ public class Hero : MonoBehaviour {
 	public float	speed = 30;
 	public float	rollMult = -45;
 	public float  	pitchMult=30;
-
-	public float	shieldLevel=1;
+    [SerializeField]
+	public float	_shieldLevel=1;
 
 	public bool	_____________________;
 	public Bounds bounds;
@@ -48,8 +48,50 @@ public class Hero : MonoBehaviour {
 		transform.rotation =Quaternion.Euler(yAxis*pitchMult, xAxis*rollMult,0);
 	}
 
+    public GameObject lastTriggerGo = null;
+
     void OnTriggerEnter(Collider other)
     {
-        print("Triggered: " + other.gameObject.name);
+        GameObject go = Utils.FindTaggedParent(other.gameObject);
+        if(go != null)
+        {
+            if (go == lastTriggerGo)
+            {
+                return;
+            }
+            lastTriggerGo = go;
+
+            if (go.tag == "Enemy")
+            {
+                shieldLevel--;
+                Destroy(go);
+            }
+            else
+            {
+                print("Triggered: " + go.name);
+            }
+            
+        }
+        else
+        {
+            print("Triggered: " + other.gameObject.name);
+        }
+        
+    }
+
+    public float shieldLevel
+    {
+        get
+        {
+            return (_shieldLevel);
+        }
+        set
+        {
+            _shieldLevel = Mathf.Min(value, 4);
+            if(value < 0)
+            {
+                Destroy(this.gameObject);
+            }
+        }
     }
 }
