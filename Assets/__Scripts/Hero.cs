cee9ab42e2ecc2a5452d951a5fe2069ff0035326
MJC504 <MJC504@CILABS-119A-09.scilsnet.rutgers.edu>
2017-10-27 19:02:39 -0400

    Added weapon and projectile scripts, creted "weapon" object in hierarchy

--- Assets/__Scripts/Hero.cs
+++ Assets/__Scripts/Hero.cs
@@ -5,6 +5,8 @@ public class Hero : MonoBehaviour {
 
 	static public Hero		S;
 
+    public float gameRestartDelay = 2f;
+
 	public float	speed = 30;
 	public float	rollMult = -45;
 	public float  	pitchMult=30;
@@ -93,6 +95,7 @@ public class Hero : MonoBehaviour {
             if(value < 0)
             {
                 Destroy(this.gameObject);
+                Main.S.DelayedRestart(gameRestartDelay);
             }
         }
     }
